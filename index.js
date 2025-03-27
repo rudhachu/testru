edf152110fc15c4fc5d0f1b0ffbbcd14a9f21573
rudhachu <120007671+rudhachu@users.noreply.github.com>
2025-03-28 00:18:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,82 +1,10 @@
-const fs = require("fs").promises;
-const path = require("path");
-const express = require("express");
-const config = require("./config");
-const { connect, connectSession, parseDir } = require("./lib");
-const { getandRequirePlugins } = require("./lib/database/plugins");
-
-class BotSystem {
-   constructor() {
-      global.__basedir = __dirname;
-      this.app = express();
-      this.port = process.env.PORT || 3000;
-   }
-
-   async initialize() {
-      try {
-         await connectSession(config.SESSION_ID, "/lib/session");
-         console.log("Version:", require("./package.json").version);
-
-         console.log("WhatsApp Bot Initializing...");
-         await parseDir(path.join(__dirname, "/lib/database/"));
-         console.log("Syncing Database");
-         await config.DATABASE.sync();
-         await parseDir(path.join(__dirname, "/plugins/"));
-         await getandRequirePlugins();
-         console.log("External Modules Installed");
-         return await connect();
-      } catch (error) {
-         console.error("Initialization error:", error);
-      }
-   }
-
-   async startServer() {
-      this.app.get("/", (req, res) => {
-         res.send("Bot Running");
-      });
-
-      this.app.listen(this.port, () => {});
-   }
-
-   async tempDir() {
-      const dir = path.join(__dirname, "temp");
-      try {
-         await fs.access(dir);
-      } catch (err) {
-         if (err.code === "ENOENT") {
-            await fs.mkdir(dir, { recursive: true });
-         }
-      }
-   }
-
-   async createGitignore() {
-      const content = `node_modules
-.gitignore
-session
-.env
-package-lock.json
-database.db`;
-
-      fs.writeFile(".gitignore", content, err => {
-         if (err) {
-            console.error("Error creating .gitignore file:", err);
-         } else {
-            console.log(".gitignore file created successfully!");
-         }
-      });
-   }
-
-   async main() {
-      try {
-         await this.initialize();
-         await this.startServer();
-         await this.tempDir();
-         await this.createGitignore();
-      } catch (error) {
-         console.warn("BOT SYSTEM FAILED");
-      }
-   }
+const client = require('./lib/client')
+const initialize = async () => {
+	try {
+		await client.connect()
+	} catch (error) {
+		console.error(error)
+	}
 }
 
-const botSystem = new BotSystem();
-botSystem.main();
+initialize()
